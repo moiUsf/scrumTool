295dfee1a8461f20b2012d8b8d18f71356dbc486
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2016-12-30 21:24:39 +0100

    neues Projekt anlegen

--- app/controllers/projekts_controller.rb
+++ app/controllers/projekts_controller.rb
@@ -1,6 +1,10 @@
 class ProjektsController < ApplicationController
+	before_action :find_item, only: [:show, :edit, :update, :destroy, :complete]
 	before_action :authenticate_user!
 	def index
+		
+	end
 
+	def new
 	end
 end
