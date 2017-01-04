82b089ec9d207bb2bdbc843959dd79ba6ec927db
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2017-01-04 17:07:58 +0100

    user story läuft - unterpunkte vom Projekt können implementiert werden

--- app/controllers/user_storys_controller.rb
+++ app/controllers/user_storys_controller.rb
@@ -1,8 +1,18 @@
 class UserStorysController < ApplicationController
 	def index
+		@userstory = UserStory.all
 	end
 
 	def new
-		@userStory = UserStory.new
+		@userstory = UserStory.new
+	end
+
+	def create
+		@userstory = UserStory.new
+    	if @userstory.save
+      		redirect_to user_storys_path
+    	else
+     		render 'new'  #wenn es nicht klappt, die view new wird nochmal mit den eingegebenen Daten geladen, noch mal versuchen
+    	end
 	end
 end
