8ed18904ce215a47fdd7f88ecfd4a7747de410d0
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2017-01-06 16:00:23 +0100

    rails neue schreibweise :projekt_id

--- app/controllers/user_storys_controller.rb
+++ app/controllers/user_storys_controller.rb
@@ -1,5 +1,6 @@
 class UserStorysController < ApplicationController
-	before_action :find_userstorie, :getProjekt, only: [:show, :edit, :update, :destroy]
+	before_action :find_userstorie, only: [:show, :edit, :update, :destroy]
+	#before_action :find_projekt, only: [:new, :show, :edit, :update, :destroy]
 	before_action :authenticate_user!
 
 	def index
@@ -12,14 +13,18 @@ class UserStorysController < ApplicationController
 
 
 	def new
+		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
 		@user_stories =  projekt.user_stories.build #UserStory.new
 	end
 
 	def create
+		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
 		@userstory = projekt.user_stories.build(userstorie_params) #UserStory.new(userstorie_params)
     	if @userstory.save
     		
-      		redirect_to user_storys_path(:projekt_id => @projekt_id)
+      		redirect_to user_storys_path(projekt_id: @projekt_id)
     	else
      		render 'new'  
     	end
@@ -27,26 +32,34 @@ class UserStorysController < ApplicationController
 
 
 	def show
+		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
   	end
 
   	def edit
+  		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
 	end
 
 	def update
+		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
 		if @userstory.update(userstorie_params)
-			redirect_to user_story_path(@userstory,:projekt_id => @projekt_id)
+			redirect_to user_story_path(@userstory,projekt_id: @projekt_id)
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
+		projekt = Projekt.find(params[:projekt_id])
+		@projekt_id = projekt.id
 		@userstory.destroy
-  		redirect_to user_storys_path(:projekt_id => @projekt_id)
+  		redirect_to user_storys_path(projekt_id: @projekt_id)
 	end
 
 
-	def getProjekt
+	def find_projekt
 		projekt = Projekt.find(params[:projekt_id])
 		@projekt_id = projekt.id
 	end
