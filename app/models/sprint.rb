cc98758147fa7f7058a16befdfe27c023ae66cc9
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2017-01-10 21:55:54 +0100

    userstory läuft wieder ;-)

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,4 +1,4 @@
 class Sprint < ApplicationRecord
-	has_many :user_stories
+	
 	belongs_to :projekt
 end
--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -1,4 +1,4 @@
 class UserStory < ApplicationRecord
 	belongs_to :projekt
-	belongs_to :sprint
+	
 end
