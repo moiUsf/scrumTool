63919706a3b803cb6dd8d4c53543750c2c3ed78a
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2017-01-11 23:03:54 +0100

    scrumboard funktionalität läuft + zurück zu sprintx

--- app/controllers/scrumboards_controller.rb
+++ app/controllers/scrumboards_controller.rb
@@ -2,7 +2,29 @@ class ScrumboardsController < ApplicationController
 	def index
 		@sprintnumber = params[:sprintnumber]
 		@sprint = Sprint.where(sprintnumber: @sprintnumber)
+		@projekt_id = params[:projekt_id]
 		@sprint_id = @sprint.ids
-		@userstories = UserStory.where(sprintnumber: @sprintnumber)
+
+		@userstoriesBack = UserStory
+								.where(sprintnumber: @sprintnumber)
+								.where(status: "backlog")
+
+
+		@userstoriesInPro = UserStory
+								.where(sprintnumber: @sprintnumber)
+								.where(status: "inProgress")
+
+
+		@userstoriesRev = UserStory
+								.where(sprintnumber: @sprintnumber)
+								.where(status: "review")
+
+
+		@userstoriesDone = UserStory
+								.where(sprintnumber: @sprintnumber)
+								.where(status: "done")
+
+
+		
 	end
 end
