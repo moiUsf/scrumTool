535c8cabbf87ed4874d67ac00e3817b573f5944d
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2017-01-11 16:49:51 +0100

    sprint index läuft

--- app/controllers/scrumboards_controller.rb
+++ app/controllers/scrumboards_controller.rb
@@ -1,2 +1,8 @@
 class ScrumboardsController < ApplicationController
+	def index
+		@sprintnumber = params[:sprintnumber]
+		@sprint = Sprint.where(sprintnumber: @sprintnumber)
+		@sprint_id = @sprint.ids
+		@userstories = UserStory.where(sprintnumber: @sprintnumber)
+	end
 end
